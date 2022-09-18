9884e9d1e5fc671ee3c189d52f3cc88416f725b8
Aleksey Andreev <fistashki22@gmail.com>
2022-09-18 19:27:23 +0300

    reinstall

--- client/scripts/helpers/utils.js
+++
@@ -1,7 +0,0 @@
-export const parseCurrentURL = () => {
-    const urlParts = {};
-
-    [urlParts.page, urlParts.id, urlParts.action] = location.hash.slice(2).split('/');
-
-    return urlParts;
-};
